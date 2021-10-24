7489444fded9755371fd074f9dd5a3894737e709
Yunus Doğan <yunus.dogan-ext@bshg.com.tr>
2021-10-24 13:06:36 +0300

    Ef and mongoDb linbs added

---
+++ Vehicle.Tracking.Core/Entities/DocumentDbEntity.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Bson;
+
+namespace Vehicle.Tracking.Core.Entities
+{
+    public abstract class DocumentDbEntity
+    {
+        public ObjectId Id { get; set; }
+        public string ObjectId => Id.ToString();
+    }
+}
